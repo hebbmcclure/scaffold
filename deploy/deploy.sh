bf6ce1d83ee978c1abb5e0664b8846251add9227
Conner Verret <connerverret@ConnerVerret.local>
2021-10-25 10:45:34 -0700

    fix: test setting deploy token as GITHUB_TOKEN in pipeline

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Find the most recent version via git tags
-PREVIOUS_VERSION=$(npx git-semver-tags | head -n 1);
+# PREVIOUS_VERSION=$(npx git-semver-tags | head -n 1);
 # Using conventional commits paradigm, check the merge commit message on master to set up a new version deployment
 if [ -n "$PREVIOUS_VERSION" ]; then BUMP_TYPE=$(npx -p conventional-changelog-angular -p conventional-recommended-bump conventional-recommended-bump --preset angular);
-    NEXT_VERSION=$(npx semver -i $BUMP_TYPE ${PREVIOUS_VERSION});
+    #NEXT_VERSION=$(npx semver -i $BUMP_TYPE ${PREVIOUS_VERSION});
     # Add a message to console with the next version number
     echo "Versioning artifacts with version $NEXT_VERSION";
     # Sets the new version for the release and deploys using the maven release profile
-    mvn -U --no-transfer-progress versions:set --define newVersion=${NEXT_VERSION};
+    # mvn -U --no-transfer-progress versions:set --define newVersion=${NEXT_VERSION};
     echo "Performing release for version $NEXT_VERSION";
     mvn -U --no-transfer-progress -P release -Dtag=${NEXT_VERSION} deploy scm:tag;
   else
