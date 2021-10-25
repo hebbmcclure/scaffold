70d00785fbbe3f830be66861ce535726da443e27
Conner Verret <connerverret@ConnerVerret.local>
2021-10-25 14:34:48 -0700

    fix: update git scm tag connection strings to use https in room pom

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 # Find the most recent version via git tags
-# PREVIOUS_VERSION=$(npx git-semver-tags | head -n 1);
+PREVIOUS_VERSION=$(npx git-semver-tags | head -n 1);
 # Using conventional commits paradigm, check the merge commit message on master to set up a new version deployment
-#if [ -n "$PREVIOUS_VERSION" ]; then BUMP_TYPE=$(npx -p conventional-changelog-angular -p conventional-recommended-bump conventional-recommended-bump --preset angular);
-    #NEXT_VERSION=$(npx semver -i $BUMP_TYPE ${PREVIOUS_VERSION});
+if [ -n "$PREVIOUS_VERSION" ]; then BUMP_TYPE=$(npx -p conventional-changelog-angular -p conventional-recommended-bump conventional-recommended-bump --preset angular);
+    NEXT_VERSION=$(npx semver -i $BUMP_TYPE ${PREVIOUS_VERSION});
     # Add a message to console with the next version number
+    echo "Versioning artifacts with version $NEXT_VERSION";
     # Sets the new version for the release and deploys using the maven release profile
-    # echo "Versioning artifacts with version $NEXT_VERSION";
-    NEXT_VERSION="9.999"
     mvn -U --no-transfer-progress versions:set --define newVersion=${NEXT_VERSION};
     echo "Performing release for version $NEXT_VERSION";
-    mvn -U --no-transfer-progress -Dtag=${NEXT_VERSION} deploy scm:tag;
-  #else
-    # echo "A problem was encountered when determining the version bump. Please check logs and try again.";
-    # return 1;
-#fi
+    mvn -U --no-transfer-progress -P release -Dtag=${NEXT_VERSION} deploy scm:tag;
+  else
+    echo "A problem was encountered when determining the version bump. Please check logs and try again.";
+    return 1;
+fi
