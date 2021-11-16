37c18724069fa4a65646b89f135b59911cf6da8f
Kyle Gress <kgress88@gmail.com>
2021-11-16 15:18:17 -0600

    re added tests

--- framework/src/test/java/io/github/kgress/scaffold/util/WebDriverValidationUtilTests.java
+++ framework/src/test/java/io/github/kgress/scaffold/util/WebDriverValidationUtilTests.java
@@ -1,190 +1,190 @@
-//package io.github.kgress.scaffold.util;
-//
-//import io.github.kgress.scaffold.BaseUnitTest;
-//import io.github.kgress.scaffold.environment.config.DesiredCapabilitiesConfigurationProperties;
-//import io.github.kgress.scaffold.exception.WebDriverManagerException;
-//import io.github.kgress.scaffold.models.enums.desktop.BrowserType;
-//import io.github.kgress.scaffold.models.enums.desktop.Platform;
-//import io.github.kgress.scaffold.models.enums.desktop.RunType;
-//import io.github.kgress.scaffold.models.enums.desktop.ScreenResolution;
-//import io.github.kgress.scaffold.models.enums.mobileemulator.MobileBrowserName;
-//import io.github.kgress.scaffold.models.enums.mobileemulator.MobilePlatform;
-//import io.github.kgress.scaffold.models.enums.mobileemulator.SauceDeviceName;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class WebDriverValidationUtilTests extends BaseUnitTest {
-//
-//  private final static String TEST_BINARY_PATH = "/tmp/headless-chromium";
-//  private final static String TEST_ACCESS_KEY = "testAccessKey";
-//  private final static String TEST_USERNAME = "testUsername";
-//  private final DesiredCapabilitiesConfigurationProperties caps = new DesiredCapabilitiesConfigurationProperties();
-//
-//  @Test
-//  public void testAwsCaps_emptyBinaryPathAndBrowser_fails() {
-//    caps.setRunType(RunType.AWS_LAMBDA_REMOTE);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateAwsLambdaDesiredCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testAwsCaps_emptyBrowser_fails() {
-//    caps.setRunType(RunType.AWS_LAMBDA_REMOTE);
-//    caps.getAwsLambda().setBrowserBinaryPath(TEST_BINARY_PATH);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateAwsLambdaDesiredCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testAwsCaps_succeeds() {
-//    caps.setRunType(RunType.AWS_LAMBDA_REMOTE);
-//    caps.setBrowserType(BrowserType.CHROME);
-//    caps.getAwsLambda().setBrowserBinaryPath(TEST_BINARY_PATH);
-//    assertDoesNotThrow(() ->
-//        WebDriverValidationUtil.validateAwsLambdaDesiredCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testDesktopCaps_emptyRunPlatform_fails() {
-//    caps.setRunType(RunType.LOCAL);
-//    caps.setBrowserType(BrowserType.CHROME);
-//    caps.setScreenResolution(ScreenResolution.SIZE_1024x1080);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testDesktopCaps_emptyBrowserType_fails() {
-//    caps.setRunType(RunType.LOCAL);
-//    caps.setRunPlatform(Platform.Mac);
-//    caps.setScreenResolution(ScreenResolution.SIZE_1024x1080);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testDesktopCaps_emptyRunType() {
-//    caps.setRunPlatform(Platform.Mac);
-//    caps.setBrowserType(BrowserType.CHROME);
-//    caps.setScreenResolution(ScreenResolution.SIZE_1024x1080);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
-//  }
-//
-//  /**
-//   * {@link DesiredCapabilitiesConfigurationProperties#getScreenResolution()} has a default set to
-//   * {@link ScreenResolution#SIZE_1024x768}.
-//   */
-//  @Test
-//  public void testDesktopCaps_emptyScreenResolution_succeeds() {
-//    caps.setRunType(RunType.LOCAL);
-//    caps.setBrowserType(BrowserType.CHROME);
-//    caps.setRunPlatform(Platform.Mac);
-//    assertEquals(ScreenResolution.SIZE_1024x1080, caps.getScreenResolution());
-//    assertDoesNotThrow(() ->
-//        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testDesktopCaps_success() {
-//    caps.setRunType(RunType.LOCAL);
-//    caps.setRunPlatform(Platform.Mac);
-//    caps.setBrowserType(BrowserType.CHROME);
-//    caps.setScreenResolution(ScreenResolution.SIZE_1024x1080);
-//    assertDoesNotThrow(() ->
-//        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testMobileCaps_emptyDeviceName_fails() {
-//    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
-//    caps.getMobile().setPlatformName(MobilePlatform.ANDROID);
-//    caps.getMobile().setBrowserName(MobileBrowserName.CHROME);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testMobileCaps_emptyBrowserName_fails() {
-//    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
-//    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
-//    caps.getMobile().setPlatformName(MobilePlatform.ANDROID);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testMobileCaps_emptyPlatformName_fails() {
-//    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
-//    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
-//    caps.getMobile().setBrowserName(MobileBrowserName.CHROME);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testMobileCaps_succeeds() {
-//    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
-//    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
-//    caps.getMobile().setPlatformName(MobilePlatform.ANDROID);
-//    caps.getMobile().setBrowserName(MobileBrowserName.CHROME);
-//    assertDoesNotThrow(() ->
-//        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testMobileCaps_androidSafari_fails() {
-//    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
-//    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
-//    caps.getMobile().setPlatformName(MobilePlatform.ANDROID);
-//    caps.getMobile().setBrowserName(MobileBrowserName.SAFARI);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testMobileCaps_iosChrome_fails() {
-//    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
-//    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
-//    caps.getMobile().setPlatformName(MobilePlatform.IOS);
-//    caps.getMobile().setBrowserName(MobileBrowserName.CHROME);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testMobileCaps_iosAndroid_fails() {
-//    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
-//    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
-//    caps.getMobile().setPlatformName(MobilePlatform.IOS);
-//    caps.getMobile().setBrowserName(MobileBrowserName.ANDROID);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
-//  }
-//
-//  @Test
-//  public void testSauceCaps_emptyUsername_fails() {
-//    caps.setRunType(RunType.SAUCE);
-//    caps.getSauce().setAccessKey(TEST_ACCESS_KEY);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredSauceAuth(caps));
-//  }
-//
-//  @Test
-//  public void testSauceCaps_emptyAccessKey_fails() {
-//    caps.setRunType(RunType.SAUCE);
-//    caps.getSauce().setUserName(TEST_USERNAME);
-//    assertThrows(WebDriverManagerException.class, () ->
-//        WebDriverValidationUtil.validateRequiredSauceAuth(caps));
-//  }
-//
-//  @Test
-//  public void testSauceCaps_succeeds() {
-//    caps.setRunType(RunType.SAUCE);
-//    caps.getSauce().setUserName(TEST_USERNAME);
-//    caps.getSauce().setAccessKey(TEST_ACCESS_KEY);
-//    assertDoesNotThrow(() ->
-//        WebDriverValidationUtil.validateRequiredSauceAuth(caps));
-//  }
-//}
+package io.github.kgress.scaffold.util;
+
+import io.github.kgress.scaffold.BaseUnitTest;
+import io.github.kgress.scaffold.environment.config.DesiredCapabilitiesConfigurationProperties;
+import io.github.kgress.scaffold.exception.WebDriverManagerException;
+import io.github.kgress.scaffold.models.enums.desktop.BrowserType;
+import io.github.kgress.scaffold.models.enums.desktop.Platform;
+import io.github.kgress.scaffold.models.enums.desktop.RunType;
+import io.github.kgress.scaffold.models.enums.desktop.ScreenResolution;
+import io.github.kgress.scaffold.models.enums.mobileemulator.MobileBrowserName;
+import io.github.kgress.scaffold.models.enums.mobileemulator.MobilePlatform;
+import io.github.kgress.scaffold.models.enums.mobileemulator.SauceDeviceName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WebDriverValidationUtilTests extends BaseUnitTest {
+
+  private final static String TEST_BINARY_PATH = "/tmp/headless-chromium";
+  private final static String TEST_ACCESS_KEY = "testAccessKey";
+  private final static String TEST_USERNAME = "testUsername";
+  private final DesiredCapabilitiesConfigurationProperties caps = new DesiredCapabilitiesConfigurationProperties();
+
+  @Test
+  public void testAwsCaps_emptyBinaryPathAndBrowser_fails() {
+    caps.setRunType(RunType.AWS_LAMBDA_REMOTE);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateAwsLambdaDesiredCapabilities(caps));
+  }
+
+  @Test
+  public void testAwsCaps_emptyBrowser_fails() {
+    caps.setRunType(RunType.AWS_LAMBDA_REMOTE);
+    caps.getAwsLambda().setBrowserBinaryPath(TEST_BINARY_PATH);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateAwsLambdaDesiredCapabilities(caps));
+  }
+
+  @Test
+  public void testAwsCaps_succeeds() {
+    caps.setRunType(RunType.AWS_LAMBDA_REMOTE);
+    caps.setBrowserType(BrowserType.CHROME);
+    caps.getAwsLambda().setBrowserBinaryPath(TEST_BINARY_PATH);
+    assertDoesNotThrow(() ->
+        WebDriverValidationUtil.validateAwsLambdaDesiredCapabilities(caps));
+  }
+
+  @Test
+  public void testDesktopCaps_emptyRunPlatform_fails() {
+    caps.setRunType(RunType.LOCAL);
+    caps.setBrowserType(BrowserType.CHROME);
+    caps.setScreenResolution(ScreenResolution.SIZE_1024x1080);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
+  }
+
+  @Test
+  public void testDesktopCaps_emptyBrowserType_fails() {
+    caps.setRunType(RunType.LOCAL);
+    caps.setRunPlatform(Platform.Mac);
+    caps.setScreenResolution(ScreenResolution.SIZE_1024x1080);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
+  }
+
+  @Test
+  public void testDesktopCaps_emptyRunType() {
+    caps.setRunPlatform(Platform.Mac);
+    caps.setBrowserType(BrowserType.CHROME);
+    caps.setScreenResolution(ScreenResolution.SIZE_1024x1080);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
+  }
+
+  /**
+   * {@link DesiredCapabilitiesConfigurationProperties#getScreenResolution()} has a default set to
+   * {@link ScreenResolution#SIZE_1024x768}.
+   */
+  @Test
+  public void testDesktopCaps_emptyScreenResolution_succeeds() {
+    caps.setRunType(RunType.LOCAL);
+    caps.setBrowserType(BrowserType.CHROME);
+    caps.setRunPlatform(Platform.Mac);
+    assertEquals(ScreenResolution.SIZE_1024x1080, caps.getScreenResolution());
+    assertDoesNotThrow(() ->
+        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
+  }
+
+  @Test
+  public void testDesktopCaps_success() {
+    caps.setRunType(RunType.LOCAL);
+    caps.setRunPlatform(Platform.Mac);
+    caps.setBrowserType(BrowserType.CHROME);
+    caps.setScreenResolution(ScreenResolution.SIZE_1024x1080);
+    assertDoesNotThrow(() ->
+        WebDriverValidationUtil.validateRequiredDesktopBrowserCapabilities(caps));
+  }
+
+  @Test
+  public void testMobileCaps_emptyDeviceName_fails() {
+    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
+    caps.getMobile().setPlatformName(MobilePlatform.ANDROID);
+    caps.getMobile().setBrowserName(MobileBrowserName.CHROME);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
+  }
+
+  @Test
+  public void testMobileCaps_emptyBrowserName_fails() {
+    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
+    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
+    caps.getMobile().setPlatformName(MobilePlatform.ANDROID);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
+  }
+
+  @Test
+  public void testMobileCaps_emptyPlatformName_fails() {
+    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
+    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
+    caps.getMobile().setBrowserName(MobileBrowserName.CHROME);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
+  }
+
+  @Test
+  public void testMobileCaps_succeeds() {
+    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
+    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
+    caps.getMobile().setPlatformName(MobilePlatform.ANDROID);
+    caps.getMobile().setBrowserName(MobileBrowserName.CHROME);
+    assertDoesNotThrow(() ->
+        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
+  }
+
+  @Test
+  public void testMobileCaps_androidSafari_fails() {
+    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
+    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
+    caps.getMobile().setPlatformName(MobilePlatform.ANDROID);
+    caps.getMobile().setBrowserName(MobileBrowserName.SAFARI);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
+  }
+
+  @Test
+  public void testMobileCaps_iosChrome_fails() {
+    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
+    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
+    caps.getMobile().setPlatformName(MobilePlatform.IOS);
+    caps.getMobile().setBrowserName(MobileBrowserName.CHROME);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
+  }
+
+  @Test
+  public void testMobileCaps_iosAndroid_fails() {
+    caps.setRunType(RunType.SAUCE_MOBILE_EMULATOR);
+    caps.getMobile().setSauceDeviceName(SauceDeviceName.SAMSUNG_GALAXY_S9_HD);
+    caps.getMobile().setPlatformName(MobilePlatform.IOS);
+    caps.getMobile().setBrowserName(MobileBrowserName.ANDROID);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredMobileEmulatorCapabilities(caps));
+  }
+
+  @Test
+  public void testSauceCaps_emptyUsername_fails() {
+    caps.setRunType(RunType.SAUCE);
+    caps.getSauce().setAccessKey(TEST_ACCESS_KEY);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredSauceAuth(caps));
+  }
+
+  @Test
+  public void testSauceCaps_emptyAccessKey_fails() {
+    caps.setRunType(RunType.SAUCE);
+    caps.getSauce().setUserName(TEST_USERNAME);
+    assertThrows(WebDriverManagerException.class, () ->
+        WebDriverValidationUtil.validateRequiredSauceAuth(caps));
+  }
+
+  @Test
+  public void testSauceCaps_succeeds() {
+    caps.setRunType(RunType.SAUCE);
+    caps.getSauce().setUserName(TEST_USERNAME);
+    caps.getSauce().setAccessKey(TEST_ACCESS_KEY);
+    assertDoesNotThrow(() ->
+        WebDriverValidationUtil.validateRequiredSauceAuth(caps));
+  }
+}
--- framework/src/test/java/io/github/kgress/scaffold/webdriverwrapper/WebDriverWrapperTests.java
+++ framework/src/test/java/io/github/kgress/scaffold/webdriverwrapper/WebDriverWrapperTests.java
@@ -1,107 +1,107 @@
-//package io.github.kgress.scaffold.webdriverwrapper;
-//
-//import io.github.kgress.scaffold.BaseUnitTest;
-//import io.github.kgress.scaffold.SharedTestVariables;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.TimeoutException;
-//import org.openqa.selenium.WebElement;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.Mockito.*;
-//
-//public class WebDriverWrapperTests extends BaseUnitTest {
-//
-//    public final static String TEST_CSS_SELECTOR2 = ".element2";
-//    public final static String TEST_URL = "https://someurl.com";
-//    private TestWebDriverWrapper testWebDriverWrapper;
-//
-//    @Mock
-//    private WebElement mockWebElement1;
-//
-//    @Mock
-//    private WebElement mockWebElement2;
-//
-//    @BeforeEach
-//    public void setup() {
-//        testWebDriverWrapper = createTestWebDriverWrapper();
-//    }
-//
-//    @Test
-//    public void testFindElement() {
-//        when(mockWebElement1.getText()).thenReturn(SharedTestVariables.TEXT_1);
-//        when(mockBaseWebDriver.findElement(By.cssSelector(SharedTestVariables.CSS_SELECTOR1)))
-//                .thenReturn(mockWebElement1);
-//        assertEquals(SharedTestVariables.TEXT_1,
-//                testWebDriverWrapper.findElement(By.cssSelector(SharedTestVariables.CSS_SELECTOR1)).getText());
-//    }
-//
-//    @Test
-//    public void testFindElements() {
-//        when(mockWebElement1.getText()).thenReturn(SharedTestVariables.TEXT_1);
-//        when(mockWebElement2.getText()).thenReturn(SharedTestVariables.TEXT_2);
-//        List<WebElement> elementList = new ArrayList<>();
-//        elementList.add(mockWebElement1);
-//        elementList.add(mockWebElement2);
-//        when(mockBaseWebDriver.findElements(By.cssSelector(SharedTestVariables.CSS_SELECTOR1)))
-//                .thenReturn(elementList);
-//        when(mockBaseWebDriver.findElements(By.cssSelector(TEST_CSS_SELECTOR2))).thenReturn(elementList);
-//        assertEquals(SharedTestVariables.TEXT_1, testWebDriverWrapper.
-//                findElements(By.cssSelector(SharedTestVariables.CSS_SELECTOR1))
-//                .get(0)
-//                .getText());
-//        assertEquals(SharedTestVariables.TEXT_2, testWebDriverWrapper
-//                .findElements(By.cssSelector(TEST_CSS_SELECTOR2))
-//                .get(1)
-//                .getText());
-//    }
-//
-//    @Test
-//    public void testGet() {
-//        mockWebDriverWrapper.get(TEST_URL);
-//        verify(mockWebDriverWrapper, times(1)).get(TEST_URL);
-//    }
-//    @Test
-//    public void testCurrentUrl() {
-//        when(mockBaseWebDriver.getCurrentUrl()).thenReturn(TEST_URL);
-//        assertEquals(TEST_URL, testWebDriverWrapper.getCurrentUrl());
-//    }
-//
-//    @Test
-//    public void testFindElementDoesntExist() {
-//        when(mockBaseWebDriver.findElement(any())).thenThrow(TimeoutException.class);
-//        assertThrows(TimeoutException.class, () -> testWebDriverWrapper.findElement(By.id("elementDoesNotExist")));
-//    }
-//
-//    @Test
-//    public void testGetPageSource() {
-//        var testPageSource = "<html><div></div><body></body></html>";
-//        when(mockBaseWebDriver.getPageSource()).thenReturn(testPageSource);
-//        assertEquals(testPageSource, testWebDriverWrapper.getPageSource());
-//    }
-//
-//    @Test
-//    public void testGetTitle() {
-//        var testTitle = "HAAAANNNNNNN SOOOLLLOOOOOOOOO!";
-//        when(mockBaseWebDriver.getTitle()).thenReturn(testTitle);
-//        assertEquals(testTitle, testWebDriverWrapper.getTitle());
-//    }
-//
-//    @Test
-//    public void testSetAutomationWaitTimeout() {
-//        var tenSeconds = 10L;
-//        testWebDriverWrapper.getAutomationWait().setTimeoutInSeconds(tenSeconds);
-//        var automationWaitInSeconds = testWebDriverWrapper.getAutomationWait().getTimeoutInSeconds();
-//        assertEquals(tenSeconds, automationWaitInSeconds);
-//    }
-//
-//    protected TestWebDriverWrapper createTestWebDriverWrapper() {
-//        return new TestWebDriverWrapper(mockBaseWebDriver, 1L);
-//    }
-//}
+package io.github.kgress.scaffold.webdriverwrapper;
+
+import io.github.kgress.scaffold.BaseUnitTest;
+import io.github.kgress.scaffold.SharedTestVariables;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+public class WebDriverWrapperTests extends BaseUnitTest {
+
+    public final static String TEST_CSS_SELECTOR2 = ".element2";
+    public final static String TEST_URL = "https://someurl.com";
+    private TestWebDriverWrapper testWebDriverWrapper;
+
+    @Mock
+    private WebElement mockWebElement1;
+
+    @Mock
+    private WebElement mockWebElement2;
+
+    @BeforeEach
+    public void setup() {
+        testWebDriverWrapper = createTestWebDriverWrapper();
+    }
+
+    @Test
+    public void testFindElement() {
+        when(mockWebElement1.getText()).thenReturn(SharedTestVariables.TEXT_1);
+        when(mockBaseWebDriver.findElement(By.cssSelector(SharedTestVariables.CSS_SELECTOR1)))
+                .thenReturn(mockWebElement1);
+        assertEquals(SharedTestVariables.TEXT_1,
+                testWebDriverWrapper.findElement(By.cssSelector(SharedTestVariables.CSS_SELECTOR1)).getText());
+    }
+
+    @Test
+    public void testFindElements() {
+        when(mockWebElement1.getText()).thenReturn(SharedTestVariables.TEXT_1);
+        when(mockWebElement2.getText()).thenReturn(SharedTestVariables.TEXT_2);
+        List<WebElement> elementList = new ArrayList<>();
+        elementList.add(mockWebElement1);
+        elementList.add(mockWebElement2);
+        when(mockBaseWebDriver.findElements(By.cssSelector(SharedTestVariables.CSS_SELECTOR1)))
+                .thenReturn(elementList);
+        when(mockBaseWebDriver.findElements(By.cssSelector(TEST_CSS_SELECTOR2))).thenReturn(elementList);
+        assertEquals(SharedTestVariables.TEXT_1, testWebDriverWrapper.
+                findElements(By.cssSelector(SharedTestVariables.CSS_SELECTOR1))
+                .get(0)
+                .getText());
+        assertEquals(SharedTestVariables.TEXT_2, testWebDriverWrapper
+                .findElements(By.cssSelector(TEST_CSS_SELECTOR2))
+                .get(1)
+                .getText());
+    }
+
+    @Test
+    public void testGet() {
+        mockWebDriverWrapper.get(TEST_URL);
+        verify(mockWebDriverWrapper, times(1)).get(TEST_URL);
+    }
+    @Test
+    public void testCurrentUrl() {
+        when(mockBaseWebDriver.getCurrentUrl()).thenReturn(TEST_URL);
+        assertEquals(TEST_URL, testWebDriverWrapper.getCurrentUrl());
+    }
+
+    @Test
+    public void testFindElementDoesntExist() {
+        when(mockBaseWebDriver.findElement(any())).thenThrow(TimeoutException.class);
+        assertThrows(TimeoutException.class, () -> testWebDriverWrapper.findElement(By.id("elementDoesNotExist")));
+    }
+
+    @Test
+    public void testGetPageSource() {
+        var testPageSource = "<html><div></div><body></body></html>";
+        when(mockBaseWebDriver.getPageSource()).thenReturn(testPageSource);
+        assertEquals(testPageSource, testWebDriverWrapper.getPageSource());
+    }
+
+    @Test
+    public void testGetTitle() {
+        var testTitle = "HAAAANNNNNNN SOOOLLLOOOOOOOOO!";
+        when(mockBaseWebDriver.getTitle()).thenReturn(testTitle);
+        assertEquals(testTitle, testWebDriverWrapper.getTitle());
+    }
+
+    @Test
+    public void testSetAutomationWaitTimeout() {
+        var tenSeconds = 10L;
+        testWebDriverWrapper.getAutomationWait().setTimeoutInSeconds(tenSeconds);
+        var automationWaitInSeconds = testWebDriverWrapper.getAutomationWait().getTimeoutInSeconds();
+        assertEquals(tenSeconds, automationWaitInSeconds);
+    }
+
+    protected TestWebDriverWrapper createTestWebDriverWrapper() {
+        return new TestWebDriverWrapper(mockBaseWebDriver, 1L);
+    }
+}
